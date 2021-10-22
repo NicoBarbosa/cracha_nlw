ad6530fc47852f6d2c2918294fce23035d9ffb93
Nicolas Penante <66042022+NicoBarbosa@users.noreply.github.com>
2021-10-22 20:31:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const linksSocialMedia = {
+  github: 'NicoBarbosa',
+  youtube: 'channel/UCCfeZQwzY8s5PCcC1L5ST2g', 
+  facebook: 'nicolas.penante.1',
+  instagram: 'nicolasspenante',
+  twitter: 'nicolasspenante'
+}
+
+
+
+function changeSocialMediaLinks() {
+  
+  for(let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+
+  }
+
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    userImage.src = data.avatar_url
+    userName.textContent = data.name
+    userLogin.textContent = data.login
+    userLink.href = data.html_url
+    userBio.textContent = data.bio
+  })
+}
+
+getGitHubProfileInfos()
+
